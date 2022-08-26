9bd0ffc49c78aa416da0a0fef430e1d4d4ca86e5
czbczb <czb2402@163.com>
2022-08-26 22:45:08 +0800

    update

---
+++ src/components/designer/EditContentItem.vue
@@ -0,0 +1,37 @@
+<template>
+  <div ref="editItem" :contenteditable="readOnly" v-menu="" v-bind="data.value"></div>
+</template>
+
+<script>
+import { reactive, toRefs, onBeforeMount, onMounted, onBeforeUpdate } from "vue";
+export default {
+  name: "EditContentItem",
+
+  props: {
+    config: Object,
+    readOnly: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  setup() {
+    const data = reactive({
+      value: "",
+    });
+    onBeforeMount(() => {
+      console.log("2.组件挂载页面之前执行----onBeforeMount");
+    });
+    onMounted(() => {
+      console.log("3.-组件挂载到页面之后执行-------onMounted");
+    });
+    onBeforeUpdate(()=> {
+        console.log(this.$refs.editItem);
+    })
+    return {
+      ...toRefs(data),
+    };
+  },
+  methods: {},
+};
+</script>
+<style scoped lang="scss"></style>
