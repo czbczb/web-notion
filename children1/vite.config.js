274790b1b1558b1e62b03682f0a08f429180ff23
zongbao.cui <zongbao.cui@mobvista.com>
2023-06-15 19:29:44 +0800

    update

--- children1/vite.config.js
+++ children1/vite.config.js
@@ -3,15 +3,23 @@ import vue from "@vitejs/plugin-vue";
 import qiankun from "vite-plugin-qiankun";
 
 // https://vitejs.dev/config/
+// const publicPath = process.env.NODE_ENV === 'production' ? './' : '/children1/'
+
 export default defineConfig({
+  publicPath: process.env.NODE_ENV === "production" ? "/children1/" : "./",
+  base: process.env.NODE_ENV === "production" ? "/children1/" : "./",
   plugins: [
     vue(),
     qiankun("app-vue3", {
       useDevMode: true,
     }),
   ],
-  base: process.env.NODE_ENV === "production" ? "/children1/" : "/",
 
+  // resolve: {
+  //   alias: {
+  //     "@": "/src",
+  //   },
+  // },
   server: {
     port: 8080,
     // 本地调试 放行
@@ -19,20 +27,55 @@ export default defineConfig({
       "Access-Control-Allow-Origin": "*",
     },
   },
-  build: {
-    lib: {
-      entry: "src/main.js",
-      name: "children1",
-      fileName: "children1",
-      formats: ["umd"],
-    },
-    rollupOptions: {
-      output: {
-        // 指定 UMD 格式的全局变量名
-        globals: {
-          vue: "Vue",
-        },
-      },
-    },
+  chainWebpack: (config) => {
+    config.plugin("define").tap((args) => {
+      args[0]["__webpack_public_path__"] = JSON.stringify("/children1/");
+      return args;
+    });
   },
+  // build: {
+  //   lib: {
+  //     entry: "src/main.js",
+  //     name: "children1",
+  //     fileName: "children1",
+  //     formats: ["umd"],
+  //   },
+  //   rollupOptions: {
+  //     output: {
+  //       // 指定 UMD 格式的全局变量名
+  //       globals: {
+  //         vue: "Vue",
+  //       },
+  //     },
+  //   },
+  // },
+  // chainWebpack: (config) => {
+  //   config.module.rule('fonts')
+  //     .use('url-loader')
+  //     .loader('url-loader')
+  //     .options({
+  //       limit: 4096, // 小于4kb将会被打包成 base64
+  //       fallback: {
+  //         loader: 'file-loader',
+  //         options: {
+  //           name: 'fonts/[name].[hash:8].[ext]',
+  //           publicPath
+  //         }
+  //       }
+  //     })
+  //     .end();
+  //   config.module.rule('images')
+  //     .use('url-loader')
+  //     .loader('url-loader')
+  //     .options({
+  //       limit: 4096, // 小于4kb将会被打包成 base64
+  //       fallback: {
+  //         loader: 'file-loader',
+  //         options: {
+  //           name: 'img/[name].[hash:8].[ext]',
+  //           publicPath
+  //         }
+  //       }
+  //     })
+  // },
 });
--- src/microConfig.js
+++ src/microConfig.js
@@ -3,14 +3,10 @@ export default {
     {
       name: "children1",
       // 修改子应用配置的entry
-
-      // 修改为部署到服务器上的index.html的入口
       entry: "//localhost:8080",
-        // process.env.NODE_ENV === "development"
-        //   ? "//localhost:8080"
-        //   : "/children/index.html",
       container: "#sub-container",
       activeRule: "/children1",
+      publicPath: "/children1",
     },
     // {
     //   name: "children2",
